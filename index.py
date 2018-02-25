b570d795c0e2a0b12ff806bb30ff00d82fe6eea0
Vasiluk Dmitrij <vasilukwolf@gmail.com>
2018-02-25 22:27:59 +0300

    Add Rules Key

--- index.py
+++ index.py
@@ -5,13 +5,29 @@ screen = pygame.display.set_mode((900, 700))
 
 finished = False
 
+x = 0
+y = 50
+
+#print pygame.K_SPACE
 
 while finished == False:
     for event in pygame.event.get():
-        if pygame.QUIT == 12:
-            finished = True
+        if event.type == 12:
+            finished = False
 
-    rectOne = pygame.Rect(0, 0, 300, 300)
+    rectOne = pygame.Rect(x, y, 30, 30)
+
+    pressedKeys = pygame.key.get_pressed()
+
+    if pressedKeys[pygame.K_LEFT] == 1:
+        x -= 1
+    if pressedKeys[pygame.K_RIGHT] == 1:
+        x += 1
+
+    if pressedKeys[pygame.K_UP] == 1:
+        y -= 1
+    if pressedKeys[pygame.K_DOWN] == 1:
+        y += 1
 
     color = (0, 0, 128)
 
