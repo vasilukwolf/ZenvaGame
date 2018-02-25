0b990f980046d66e9d3c333fbda94211ef565d0a
Vasiluk Dmitrij <vasilukwolf@gmail.com>
2018-02-26 00:00:53 +0300

    Add background and make start zone

--- index.py
+++ index.py
@@ -5,20 +5,25 @@ screen = pygame.display.set_mode((900, 700))
 
 finished = False
 
-x = 0
-y = 50
+x = 450-35/2
+y = 645
 
 playerImage = pygame.image.load("Player.png")
-playerImage = pygame.transform.scale(playerImage,(30, 30))
-playerImage = playerImage.convert()
+playerImage = pygame.transform.scale(playerImage,(50, 60))
+playerImage = playerImage.convert_alpha()
+backGroundImage = pygame.image.load("background.png")
+backGroundImage = pygame.transform.scale(backGroundImage, (900, 700))
+screen.blit(backGroundImage,(0,0))
+
 
 while finished == False:
     for event in pygame.event.get():
         if event.type == 12:
             finished = False
 
-    black = (0, 0, 0)
-    screen.fill(black)
+    #black = (0, 0, 0)
+    white = (255, 255, 255)
+    screen.blit(backGroundImage, (0, 0))
     screen.blit(playerImage, (x, y))
     rectOne = pygame.Rect(x, y, 30, 30)
     frame = pygame.time.Clock()
