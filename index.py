c8c8c2db7aa7193cccafe463b94f0be5a3c27787
Vasiluk Dmitrij <vasilukwolf@gmail.com>
2018-02-27 22:27:47 +0300

    Encapsulation collision

--- index.py
+++ index.py
@@ -1,5 +1,28 @@
 import pygame
 
+
+def checkCollision(x, y, treasureX, treasureY):
+    global screen, textWin
+    collisionState = False
+    if y >= treasureY and y <= treasureY + 40:
+        if x >= treasureX and x <= treasureX + 35:
+            y = 650
+            collisionState = True
+
+        elif x + 35 >= treasureX and x + 35 <= treasureX + 35:
+            y = 650
+            collisionState = True
+
+    elif y + 40 >= treasureY and y + 40 <= treasureY + 40:
+        if x >= treasureX and x <= treasureX + 35:
+            y = 650
+            collisionState = True
+
+        elif x + 35 >= treasureX and x + 35 <= treasureX + 35:
+            y = 650
+            collisionState = True
+    return collisionState, y
+
 pygame.init()
 screen = pygame.display.set_mode((900, 700))
 
@@ -8,8 +31,9 @@ finished = False
 x = 450 - 35 / 2
 y = 650
 
+
 font = pygame.font.SysFont("comicsans", 60)
-textWin = font.render("Yep,diamond box captured!", True, (0, 0, 0))
+textWin = font.render("Yep,you win diamond!", True, (0, 0, 0))
 treasureImage = pygame.image.load("treasure.png")
 treasureImage = pygame.transform.scale(treasureImage, (35, 40))
 treasureImage = treasureImage.convert_alpha()
@@ -36,20 +60,11 @@ while finished == False:
     screen.blit(backGroundImage, (0, 0))
     screen.blit(treasureImage, (treasureX, treasureY))
     screen.blit(playerImage, (x, y))
-
-    if y >= treasureY and y <= treasureY + 40:
-        if x >= treasureX and x <= treasureY + 35:
-            screen.blit(textWin,(450 - textWin.get_width()/2, 300 - textWin.get_height()/2))
-        elif x + 35 >= treasureX and x + 35 <= treasureX + 35:
-            screen.blit(textWin, (450 - textWin.get_width()/2, 300 - textWin.get_height()/2))
-    elif y >= treasureY - 40 and y + 40 <= treasureY + 40:
-        if x >= treasureX and x <= treasureX + 35:
-            screen.blit(textWin, (450 - textWin.get_width()/2, 300 - textWin.get_height()/2))
-        elif x + 35 > treasureX and x + 35 <= treasureX + 35:
-            screen.blit(textWin, (450 - textWin.get_width()/2, 300 - textWin.get_height()/2))
+    collosionTeasure,y = checkCollision(x,y,treasureX,treasureY)
 
     rectOne = pygame.Rect(x, y, 30, 30)
     frame = pygame.time.Clock()
+    collosionTeasure = True
     pressedKeys = pygame.key.get_pressed()
 
     if pressedKeys[pygame.K_LEFT] == 1:
